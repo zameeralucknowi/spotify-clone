29030259a101cb0d8bf49dd986d459051ffa91cd
zameer <zameeralucknowi@gmail.com>
2024-09-23 01:09:55 +0530

    added

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import PlayerContextProvider from './context/PlayerContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <PlayerContextProvider>
+    <App />
+    </PlayerContextProvider>
+    </BrowserRouter>
+  </StrictMode>,
+)
